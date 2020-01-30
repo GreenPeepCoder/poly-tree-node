4f92b1c6144f49ff6db74b8bdaf806906edba664
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-01-30 16:48:52 -0500

    Complete #initialize and #parent= according to rspec file included

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,18 @@
 class PolyTreeNode
 
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @parent = nil
+        @children = Array.new()
+        @value = value
+    end
+
+    def parent= (node)
+        if @parent != nil && node != nil
+            @parent.children.delete(self)
+        end
+        @parent = node
+        node.children << self if node != nil
+    end
 end
